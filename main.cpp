2c69a596fc31416d06e9a5a8ac220e4ce0d2784f
aPoPa <85037496+Sp0146001@users.noreply.github.com>
2025-10-23 20:34:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-	std::cout << "adadada" << "\n";
+	std::cout << "nononoon" << "\n";
 	std::cout << "hello,top-it!!!!!!" << "\n";
 	std::cout << "Your name is Dobsov Sergay\n";
 	return 0;
