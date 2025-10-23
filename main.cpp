bc726137dda7807f0621b85a0620ac0515b2e069
Sp0146001 <sp0146001@gmail.com>
2025-10-23 20:35:35 +0300

    hi top-it

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 	std::cout << "adadada" << "\n";
-	std::cout << "hello,top-it!!!!!!" << "\n";
+	std::cout << "hi,top-it!!!!!!" << "\n";
 	std::cout << "Your name is Dobsov Sergay\n";
 	return 0;
 }
